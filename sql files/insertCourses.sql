bc0996897a0064d32e4ae8c5ff211866004ed8ee
Kat <pacek@uwindsor.ca>
2022-02-07 17:42:56 -0500

    added website files

---
+++ sql files/insertCourses.sql
@@ -0,0 +1,349 @@
+INSERT INTO `courses`(
+    `id`, 
+    `course_code`, 
+    `credits`,
+    `name`
+)
+VALUES
+    (
+        1047,
+        'COMP-1047',
+        3,
+        'Computer Concepts for End-Users'
+    ),
+    (
+        1400,
+        'COMP-1400',
+        3,
+        'Introductions to Algoritms and Programming I',
+    ),
+    (
+        1410,
+        'COMP-1410',
+        3,
+        'Introduction to Algorithms and Programming II'
+    ),
+    (
+        2057,
+        'COMP-2057',
+        3,
+        'Introduction to the Internet'
+    ),
+    (
+        2067,
+        'COMP-2067',
+        3,
+        'Programming for Beginners'
+    ),
+    (
+        2077,
+        'COMP-2077',
+        3,
+        'Problem Solving and Information on the Internet'
+    ),
+    (
+        2097,
+        'COMP-2097',
+        3,
+        'Social Media Marketing for End Users'
+    ),
+    (
+        2120,
+        'COMP-2120',
+        3,
+        'Object-Oriented Programming Using Java'
+    ),
+    (
+        2140,
+        'COMP-2140',
+        3,
+        'Computer Languages, Grammers, and Translators'
+    ),
+    (
+        2310,
+        'COMP-2310',
+        3,
+        'Theoretical Foundations of Computer Science'
+    ),
+    (
+        2540,
+        'COMP-2540',
+        3,
+        'Data Structures and Algorithms'
+    ),
+    (
+        2560,
+        'COMP-2560',
+        3,
+        'Systems Programming'
+    ),
+    (
+        2650,
+        'COMP-2650',
+        3,
+        'Computer Architecture I: Digital Design'
+    ),
+    (
+        2660,
+        'COMP-2660',
+        3,
+        'Computer Architecture II: Microprocessor Programming'
+    ),
+    (
+        2707,
+        'COMP-2707',
+        3,
+        'Advanced Web Design, Construction, and Deployment'
+    ),
+    (
+        2750,
+        'COMP-2750',
+        3,
+        'Selected Topics'
+    ),
+    (
+        2800,
+        'COMP-2800',
+        3,
+        'Software Development'
+    ),
+    (
+        2980,
+        'COMP-2980',
+        3,
+        'Co-op Work Term I'
+    ),
+    (
+        3057,
+        'COMP-3057',
+        3,
+        'Cyber-Ethics'
+    ),
+    (
+        3077,
+        'COMP-3077',
+        3,
+        'Web-Based Data Management'
+    ),
+    (
+        3110,
+        'COMP-3110',
+        3,
+        'Introduction to Software Engineering'
+    ),
+    (
+        3150,
+        'COMP-3150',
+        3,
+        'Database Management Systems'
+    ),
+    (
+        3220,
+        'COMP-3220',
+        3,
+        'Object-Oriented Software Analysis and Design'
+    ),
+    (
+        3300,
+        'COMP-3300',
+        3,
+        'Operating Systems Fundamentals'
+    ),
+    (
+        3340,
+        'COMP-3340',
+        3,
+        'World Wide Web Information Systems Development'
+    ),
+    (
+        3400,
+        'COMP-3400',
+        3,
+        'Advanced Object Oriented System Design Using C++'
+    ),
+    (
+        3500,
+        'COMP-.3500',
+        3,
+        'Introduction to Multimedia Systems'
+    ),
+    (
+        3520,
+        'COMP-350',
+        3,
+        'Introduction to Computer Graphics'
+    ),
+    (
+        3540,
+        'COMP-3540',
+        3,
+        'Theory of Computation'
+    ),
+    (
+        3670,
+        'COMP-3670',
+        3,
+        'Computer Networks'
+    ),
+    (
+        3680,
+        'COMP-3680',
+        3,
+        'Network Practicum'
+    ),
+    (
+        3710,
+        'COMP-3710',
+        3,
+        'Artificial Intelligence Concepts'
+    ),
+    (
+        3750,
+        'COMP-3750',
+        3,
+        'Selected Topics'
+    ),
+    (
+        3760,
+        'COMP-3760',
+        3,
+        'Selected Topics'
+    ),
+    (
+        3770,
+        'COMP-3770',
+        3,
+        'Game Design, Development and Tools'
+    ),
+    (
+        3980,
+        'COMP-3980',
+        3,
+        'Co-op Work Term II'
+    ),
+    (
+        4110,
+        'COMP-4110',
+        3,
+        'Software Verification and Testing'
+    ),
+    (
+        4150,
+        'COMP-4150',
+        3,
+        'Advanced and Practical Database Systems'
+    ),
+    (
+        4200,
+        'COMP-4200',
+        3,
+        'Mobile Application Development'
+    ),
+    (
+        4220,
+        'COMP-4220',
+        3,
+        'Agile Software Development'
+    ),
+    (
+        4250,
+        'COMP-4250',
+        3,
+        'Big Data Analytics and Database Design'
+    ),
+    (
+        4400,
+        'COMP-4400',
+        3,
+        'Principles of Programming Languages'
+    ),
+    (
+        4500,
+        'COMP-4500',
+        3,
+        'Multimedia System Development'
+    ),
+    (
+        4540,
+        'COMP-4540',
+        3,
+        'Design and Analysis of Computer Algorithms'
+    ),
+    (
+        4670,
+        'COMP-4670',
+        3,
+        'Network Security'
+    ),
+    (
+        4680,
+        'COMP-4680',
+        3,
+        'Advanced Networking'
+    ),
+    (
+        4700,
+        'COMP-4700',
+        3,
+        'Project Using Selected Tools'
+    ),
+    (
+        4730,
+        'COMP-4730',
+        3,
+        'Advanced Topics in Artificial Intelligence I'
+    ),
+    (
+        4740,
+        'COMP-4740',
+        3,
+        'Advanced Topics in Artificial Intelligence II'
+    ),
+    (
+        4750,
+        'COMP-4750',
+        3,
+        'Selected Topics'
+    ),
+    (
+        4760,
+        'COMP-4760',
+        3,
+        'Selected Topics'
+    ),
+    (
+        4770,
+        'COMP-4770',
+        3,
+        'Artificial Intelligence for Games'
+    ),
+    (
+        4800,
+        'COMP-4800',
+        3,
+        'Selected Topics in Software Engineering'
+    ),
+    (
+        4960,
+        'COMP-4960',
+        6,
+        'Research Project'
+    ),
+    (
+        4970,
+        'COMP-4970',
+        3,
+        'Co-op Work Term III'
+    ),
+    (
+        4980,
+        'COMP-4980',
+        3,
+        'Co-op Work Term IV'
+    ),
+    (
+        4990,
+        'COMP-4990',
+        3,
+        'Project Management: Techniques and Tools'
+    );
